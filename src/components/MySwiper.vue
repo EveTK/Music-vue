567e60060542c906cb17abff93a72785fcb9da1e
EvaTK <1060404931@qq.com>
2020-08-18 08:03:02 +0800

    大致结构

---
+++ src/components/MySwiper.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="my-swiper">
+      swiper
+  </div>
+</template>
+
+<script>
+import 'swiper/dist/css/swiper.css'
+import { swiper, swiperSlide } from 'vue awesome swiper'
+
+export default {
+    props: {
+        swiperList: {
+            type: Array,
+            resquird: true
+        }
+    },
+    components: {
+        swiper,
+        swiperSlide
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Main from '@/views/Main.vue'
+import Recommend from '@/views/Recommend.vue'
+import Singer from '@/views/Singer.vue'
+import Rank from '@/views/Rank.vue'
+import Search from '@/views/Search.vue'
 
 Vue.use(VueRouter)
 
   const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: Main,
+    children: [
+      {path: '', component: Recommend},
+      { path: 'recommend',component: Recommend },
+      { path: 'singer',component: Singer },
+      { path: 'rank',component: Rank },
+      { path: 'search',component: Search },
+    ]
   }
 ]
 
